605522b954f4e60098f2f3cf0cf84ceeb595f61d
TGKElit <tgkelit@gmail.com>
2023-05-29 16:57:05 +0200

    fixed logout functionality

--- react-assignment/src/pages/Home.jsx
+++ react-assignment/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import Button from "../components/button";
 
 const Home = (props) => {
+
+  function logout() {
+    sessionStorage.clear();
+    window.location.reload(false);
+  }
+
   if (sessionStorage.getItem("api_key") !== null) {
     return [
       <>
         <h1>Quizmaster</h1>
         <Button type="link" href="/game-creation">New Game</Button>
-        <Button handleClick={() => {sessionStorage.clear();}}>Logout</Button>
+        <Button handleClick={logout}>Logout</Button>
       </>,
     ];
   } else {
